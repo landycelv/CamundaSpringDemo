411fb1e852bd8eb4c7352cc511e275227e94814c
landycelv <landycelv@163.com>
2019-12-08 19:44:24 +0800

    Initial commit

---
+++ src/main/java/com/petrochina/camundaspringdemo/CamundaspringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.petrochina.camundaspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaspringdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaspringdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petrochina/camundaspringdemo/CamundaspringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petrochina.camundaspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaspringdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
